33a12394a0578e5b28f5417d582215eb867671e7
sawyermcbrayer <90974302+sawyermcbrayer@users.noreply.github.com>
2022-12-05 18:53:08 -0500

    Delete PlayingCat directory

--- PlayingCat/src/PlayingCats.java
+++
@@ -1,7 +0,0 @@
-public class PlayingCats {
-    public static boolean isCatPlaying (boolean summer , int temperature){
-        if (!summer){
-            return temperature <= 35 && temperature >= 25;
-        } else return temperature <= 45 && temperature >= 25;
-    }
-}
